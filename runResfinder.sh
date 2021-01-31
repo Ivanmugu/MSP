cee0bea047143167a2d646b6e1e64644686b3908
Ivan Munoz Gutierrez <51939300+Ivanmugu@users.noreply.github.com>
2021-01-30 17:47:51 -0800

    Add files via upload

---
+++ runResfinder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Run resfinder with multple fasta files contained in a folder
+#
+# Accessing fasta files in input folder
+for fastaFile in $(ls ~/Desktop/cge_input)
+do
+  # Save isolate ID number to make folder for results
+  folderName=${fastaFile%%.*}
+  mkdir ~/Desktop/cge_output/$folderName
+  # run resfinder
+  python3 ~/Documents/CGE/resfinder/run_resfinder.py \
+    -o ~/Desktop/cge_output/$folderName -s "Escherichia coli" -l 0.6 -t 0.8 \
+    --acquired --point -ifa ~/Desktop/cge_input/$fastaFile
+  echo "Done $fastaFile"
+done
+
+echo "Work is done!"
